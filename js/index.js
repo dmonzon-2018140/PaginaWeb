f2ffda9e50a0d37156437114ad56db6310fd440c
Diego Monzon <dmonzon-2018140@kinal.edu.gt>
2023-01-20 10:31:24 -0600

    Primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+function saludar(){
+    let numA = document.getElementById('valorA').value;
+    let numB = document.getElementById('valorB').value;
+
+    let resultado = parseInt(numA) + parseInt(numB);
+
+    console.log('Saludos');
+    console.log(resultado);
+}
